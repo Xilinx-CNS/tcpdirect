0113ef091d8e94ce21d4d85a7563ec33c5ac99e4
Kieran Mansley <kieranm@xilinx.com>
2025-09-05 14:33:35 +0100

    ON-16400: switch zf*pingpong to use rdtscp for more accurate latency measurement

--- src/tests/zf_apps/zf_timer.h
+++ src/tests/zf_apps/zf_timer.h
@@ -9,10 +9,24 @@
 #include <x86intrin.h>
 #include <stdint.h>
 
-inline uint64_t get_frc64_time(void)
+/* Return measure of free running counter, using rdtsc, so may be reordered with other code */
+inline uint64_t get_frc64_time_standard(void)
 {
   return __rdtsc();
 }
+
+/* Return measure of free running counter, using rdtscp, which gives higher accuracy when measuring 
+ * the duration of blocks of code as it can't be reordered, but has slightly higher overhead than
+ * get_frc64_time_standard() */
+inline uint64_t get_frc64_time_accurate(void)
+{
+  unsigned int aux;
+  return __rdtscp(&aux);
+}
+
+/* Default to using the more accurate measure as all current uses of this function are using it to
+ * measure ping pong deltas, and hence will benefit from the accuracy */
+#define get_frc64_time() get_frc64_time_accurate()
 
 void init_tsc_frequency(void);
 
